f8f2b9314a9051c0f606661c04c3da6a0e2adbc3
boc5244 <31375763+boc5244@users.noreply.github.com>
2019-02-02 23:32:03 -0500

    Updates

--- MakeFunction/MakePosFunction_SOS.m
+++ MakeFunction/MakePosFunction_SOS.m
@@ -1,15 +1,13 @@
-function [] = MakeChirp(root,FI,FE,A,T,Fs,centPos,rmp,method,showplot,saveFunc)
-% MakePosFunction_Chirp: makes chirp position function
+function [] = MakePosFunction_SOS(root,F,N,A,T,Fs,centPos,showplot,saveFunc)
+% MakePosFunction_SOS: makes sum-of-sine position function
 %   INPUTS:
 %       root:       :   root directory to save position function file
-%       FI          :   start frequency [Hz]
-%       FE          :   end frequency [Hz]
+%       F           :   frequency range vector [Hz]
+%       N           :   # of frequencies
 %       A           :   amplitude (+/-) [deg]
 %       T           :   total time [s]
 %       Fs          :   sampling Frequency [Hz]
 %       centPos     :   pixel # at cneter of panel
-%       rmp         :   (1 = ramp up , -1 = ramp down)
-%       method      :   linear, logarithmic, etc. (see chirp.m)
 %       showplot  	:   boolean (1 = show pos vs time)
 %       saveFunc  	:   boolean (1 = save function to root)
 %   OUTPUTS:
@@ -18,32 +16,33 @@ function [] = MakeChirp(root,FI,FE,A,T,Fs,centPos,rmp,method,showplot,saveFunc)
 %---------------------------------------------------------------------------------------------------------------------------------
 % clear ; close all ; clc
 % root        = 'Q:\Box Sync\Research\Redundant Control\Head Frequency Response\Arena Functions\Chirp\';
-% FI          = 0.1;
-% FE          = 12;
-% A           = 7.5;
+% F           = [0.1 12];
+% N           = 15;
+% A           = 2*ones(N,1);
 % T           = 20;
 % Fs          = 100;
 % centPos     = 14;
-% rmp         = 1;
-% method      = 'Logarithmic';
 % showplot    = 1;
 %% Generate Chirp Signal %%
 %---------------------------------------------------------------------------------------------------------------------------------
 tt = (0:1/Fs:T)';  % time vector [s]
-Func.deg = A*chirp(tt,FI,T,FE,method); % chirp signal [deg]
+% f = logspace((log(F(1))/log(10)),(log(F(2))/log(10)),N)'; % frequency vector [Hz]
+f = linspace(F(1),F(2),N); % frequency vector [Hz]
+f = 0.05*round(f/0.05); % round frequencies to prime harmonics
+Phase = deg2rad(randi(359,N,1)); % phase [deg]
 
-if rmp==1
-elseif rmp==-1
-    Func.deg = flipud(Func.deg); % change direction to ramp down
-else
-    error('rmp must be 1 or -1')
+X = zeros(length(tt),1);
+for kk = 1:N
+   X = X + A(kk)*sin(2*pi*f(kk)*tt + Phase(kk)); 
 end
 
+Func.deg = X; % sum-of-sine [deg]
+
 Func.panel = 3.75*round(Func.deg/3.75); % convert deg to panel position
-% Chirp Position Plot
+% SOS Position Plot
 if showplot
-    figure ; clf ; hold on ; box on ; title('Chirp Position')
-        plot(tt,Func.deg,'k','LineWidth',2)
+    figure ; clf ; hold on ; box on ; title('SOS Position')
+        plot(tt,Func.deg,'k','LineWidth',1)
         plot(tt,Func.panel,'b','LineWidth',2)
         xlabel('Time (s)')
         legend('deg','panel')
@@ -60,24 +59,28 @@ if showplot
         xlabel('Frequency (Hz)')
         plot(Fv1,Mag1,'k','LineWidth',2);
         plot(Fv2,Mag2,'b','LineWidth',1);
-      	plot([FE FE],[0 max(Mag1)],'-r')
-        xlim([0 FE+0.1*FE])
-    	legend('deg','panel','limit')
+        xlim([0 F(2)+1])
+    	legend('deg','panel')
 
     subplot(212) ; hold on ; box on
         ylabel('Phase (rad)')
         xlabel('Frequency (Hz)')
         plot(Fv1,Phase1,'k','LineWidth',2);
         plot(Fv2,Phase2,'b','LineWidth',1);
-      	plot([FE FE],[min(Phase1) max(Phase1)],'-r')
-        xlim([0 FE+0.1*FE])
+        xlim([0 F(2)+1])
 end
 
 %% Save Fucntion %%
 %---------------------------------------------------------------------------------------------------------------------------------
 if saveFunc
-    func  = (Func.panel./3.75) + centPos;
-    fname = sprintf('position_function_%s_chirp_amp_%1.1f_freq_%1.1f_%1.1f_fs_%i_T_%1.1f.mat',method,A,FI,FE,Fs,T);
+    func  = Func.panel + centPos;
+	% Name file
+    strFreq = '';
+    for kk = 1:length(f)
+       strFreq = [strFreq  num2str(f(kk)) '_'];
+    end
+    strFreq = strtrim(strFreq);
+    fname = sprintf(['position_function_SOS_T_%1.1f_freq_' strFreq '.mat'],T);
     save([ root fname], 'func');
 end
 end
