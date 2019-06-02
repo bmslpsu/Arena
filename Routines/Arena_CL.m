dd96e450aa00cfd31d7427d714f1744662a89830
boc5244 <31375763+boc5244@users.noreply.github.com>
2019-06-02 13:56:31 -0400

    Update Arena_CL.m

--- Routines/Arena_CL.m
+++ Routines/Arena_CL.m
@@ -35,12 +35,12 @@ n_pause = 0.2;
 
 disp('rest');
 Panel_com('stop'); pause(n_pause)
-Panel_com('set_pattern_id', patID);pause(n_pause)           	% set pattern
-Panel_com('set_mode',mode); pause(n_pause)                      % closed loop tracking [xpos,ypos] (NOTE: 0=open, 1=closed)
-Panel_com('set_position',[1 1]); pause(n_pause)                 % set starting position (xpos,ypos)
-Panel_com('set_posfunc_id',[1,0]); pause(n_pause)               % no position function for x-channel
-Panel_com('set_funcX_freq', 50); pause(n_pause)                 % 50Hz update rate
-Panel_com('set_funcY_freq', 50); pause(n_pause)                 % 50Hz update rate
-Panel_com('send_gain_bias',GAIN); pause(n_pause)                % [xgain,xoffset,ygain,yoffset]
-Panel_com('start');                                             % start closed-loop rest
+Panel_com('set_pattern_id', patID);pause(n_pause)      	% set pattern
+Panel_com('set_mode',mode); pause(n_pause)             	% closed loop tracking [xpos,ypos] (NOTE: 0=open, 1=closed)
+Panel_com('set_position',[1 1]); pause(n_pause)        	% set starting position (xpos,ypos)
+Panel_com('set_posfunc_id',[1,0]); pause(n_pause)     	% no position function for x-channel
+Panel_com('set_funcX_freq', 50); pause(n_pause)       	% X update rate
+Panel_com('set_funcY_freq', 50); pause(n_pause)        	% Y update rate
+Panel_com('send_gain_bias',GAIN); pause(n_pause)      	% [xgain,xoffset,ygain,yoffset]
+Panel_com('start');                                    	% start closed-loop rest
 end
